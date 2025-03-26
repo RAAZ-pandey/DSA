fcf14e5a0bce26803404ca206ad2b9d327c1e7ad
Rajkumar pandey <Raj.11165@stu.upes.ac.in>
2025-03-26 21:52:43 +0530

    team

--- Generatealternate.js
+++ Generatealternate.js
@@ -2,5 +2,12 @@ function generateSeries() {
   let start = 1, end = 10;
   let result = [];
 
-  
+   while (start <= end) 
+   {
+    result.push(start++);
+    if (start <= end)
+      result.push(end--);
+   }
+
+   console.log(result.join(" "));
 }
