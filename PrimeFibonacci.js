845904ed784f0dc0bd20dabc46a763be9db2d913
Rajkumar pandey <Raj.11165@stu.upes.ac.in>
2025-03-27 21:33:03 +0530

    fibonacci series

---
+++ PrimeFibonacci.js
@@ -0,0 +1,30 @@
+function isPrime(num)
+{
+  if (num < 2)
+      return false;
+
+  for ( let i = 2; i*i <= num; i++)
+  {
+    if (num % i === 0)
+        return false;
+  }
+  return true;
+}
+
+function fibonacciPrimes(n)
+{
+  let a = 0, b = 1, count = 0;
+
+  while (count < n)
+  {
+    let next = a + b;
+    a = b;
+    b = next;
+
+    if (isPrime(next))
+    {
+      console.log(next);
+      count++;
+    }
+  }
+}
