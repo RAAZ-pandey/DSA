1990cc817a0f820ade8a44ba26b72d17bf2ee64b
Rajkumar pandey <Raj.11165@stu.upes.ac.in>
2025-03-31 20:58:31 +0530

    factorial series

--- Factorial.js
+++ Factorial.js
@@ -10,3 +10,13 @@ function factorial(n)
   return fact;
 }
 
+function calculateSeries(limit)
+{
+  let sum = 0;
+  for (let i = 1; i <= limit; i++)
+  {
+    sum += i / factorial(i);
+  }
+  return sum;
+}
+
