9463963a06a7545c4626476b136dd18ba12d61ae
Rajkumar pandey <Raj.11165@stu.upes.ac.in>
2025-03-31 21:12:55 +0530

    sum of digits

---
+++ SumDigit.js
@@ -0,0 +1,13 @@
+function sumOfDigits(sum)
+{
+  let sum = 0;
+  while (num > 0)
+  {
+    let lastDigit = num - (num / 10) * 10 //Extract last digit
+    while (lastDigit >= 10)lastDigit -= 10; //reduce last digit to single digit
+    sum += lastDigit;
+    num = (num - lastDigit) / 10; //remove last digit
+    while (num >= 10 && num % 10 !== num)num -= num  % 10;
+  }
+  return sum;
+}
