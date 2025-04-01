642434d8d2fc41c12f90617f65d924f35d557f47
Rajkumar pandey <Raj.11165@stu.upes.ac.in>
2025-04-01 21:48:10 +0530

    num to words

---
+++ Num_Words.js
@@ -0,0 +1,25 @@
+function numToWords(num)
+{
+  if(isNaN(num))
+  {
+    console.log("please enter a valid number");
+    return;
+  }
+
+  const words = ["Zero", "One", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine"];
+
+  let numstr = Math.abs(num).toString();
+  let result = [];
+
+  for (let digit of numstr)
+  {
+    result.push(words[parseInt(digit)]);
+  }
+
+  if (num < 0)
+  {
+    result.unshift("Minus");
+  }
+
+  console.log(result.join(" "));
+}
