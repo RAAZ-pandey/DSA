acf3a5b7c63d54ee5967467ac389761c36509c08
Rajkumar pandey <Raj.11165@stu.upes.ac.in>
2025-04-02 21:36:06 +0530

    function to sum

---
+++ FirstLastDigitSum.js
@@ -0,0 +1,10 @@
+function sumFirstLAstDigit(n)
+{
+   let lastDigit = n % 10;
+   let firstDigit = n;
+   while (firstDigit >= 10)
+   {
+    firstDigit = Math.floor(firstDigit / 10);
+   }
+   return firstDigit + lastDigit;
+}
