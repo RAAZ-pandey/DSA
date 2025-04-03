065e85ca71ba252bcb8bc4c411f7e90b77419789
Rajkumar pandey <Raj.11165@stu.upes.ac.in>
2025-04-03 23:41:38 +0530

    sequence

--- MultipleOfSequence.js
+++ MultipleOfSequence.js
@@ -1,5 +1,14 @@
 function generateSequence(n)
 {
   let a =1, b= 2;
-  
+  console.log(a);
+  console.log(b);
+
+  for (let i = 2; i < n; i++)
+  {
+    let next = a * b;
+    console.log(next);
+    a = b;
+    b =next;
+  }
 }
