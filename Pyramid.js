e7531fa34eee356111bd73051a1f873b6aa00df4
Rajkumar pandey <Raj.11165@stu.upes.ac.in>
2025-04-09 21:25:51 +0530

    pyramid

---
+++ Pyramid.js
@@ -0,0 +1,12 @@
+function printPyramid(rows) {
+  for (let i = 1; i <= rows; i++) {
+    let str = '';
+
+    // Add spaces
+    for (let space = 1; space <= rows - i; space++) {
+      str += ' ';
+    }
+
+    
+  }
+}
