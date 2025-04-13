ab274017c281a4a48f33c12adf49c083f7f9da53
Rajkumar pandey <Raj.11165@stu.upes.ac.in>
2025-04-13 22:01:25 +0530

    palindrome

---
+++ PalindromeNumber.js
@@ -0,0 +1,12 @@
+function isPalindrome(num)
+{
+  let original = num;
+  let reversed = 0;
+
+  while (num > 0)
+  {
+    let digit = num % 10;
+    reversed = reversed * 10 + digit ;
+    num = Math.floor(num / 10);
+  }
+}
