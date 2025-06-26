433efaa73b25eb66a55836e8fe0ff86dedb68f6a
Rajkumar pandey <Raj.11165@stu.upes.ac.in>
2025-06-26 23:25:24 +0530

    sum by sorting

--- LEETCODE/TwoSum.java
+++ LEETCODE/TwoSum.java
@@ -14,5 +14,35 @@ class TwoSum {
     throw new IllegalArgumentException("No two sum solution");
   }
 
+  int[] twoSumSorting(int[] nums, int target) {
+      int[] copyArray = Arrays.copyOf(nums, nums.length);
+      Arrays.sort(copyArray);
+
+      int head = 0;
+      int tail = copyArray.length - 1;
+      int num1 = 0, num2 = 0;
+      while (head < tail) {
+        int sum = copyArray[head] + copyArray[tail];
+        if (sum < target) {
+          head++;
+        }
+        else if (sum > target) {
+          tail--;
+        } else {
+          num1 = copyArray[head];
+          num2 = copyArray[tail];
+          break;
+        }
+    }
+
+    // Create the result array with indices
+    int[] result = new int[2];
+    for (int i = 0; i < nums.length; i++) {
+      if (nums[i] == num1) result[0] = i;
+      if (nums[i] == num2) result[1] = i;
+    }
+    return result;
+  }
+
  
 }
