f554d0ad6fda0c64ba09ecd1f3fc46bf1d4f270b
Rajkumar pandey <Raj.11165@stu.upes.ac.in>
2025-06-27 10:13:31 +0530

    Efficient way using hashiing

--- LEETCODE/TwoSum.java
+++ LEETCODE/TwoSum.java
@@ -44,5 +44,26 @@ class TwoSum {
     return result;
   }
 
+  int[] twoSumHashing(int[] nums, int target) {
+
+    // Create a HashMap
+    Map<Integer, Integer> map = new HashMap<>();
+
+    for (int i = 0; i < nums.length; i++) {
+
+      // Get the complement using the target value
+      int complement = target - nums[i];
+
+      // Search the hashmap for complement, if found, we got our pair
+      if (map.containsKey(complement)) {
+        return new int[]{map.get(complement), i};
+      }
+
+      // Put the element in hashmap for subsequent searches.
+      map.put(nums[i], i);
+    }
+    throw new IllegalArgumentException("No two sum solution");
+  }
+
  
 }
