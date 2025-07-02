5963bc926e0297ba3c17a254a67dddd415d145fd
Rajkumar pandey <Raj.11165@stu.upes.ac.in>
2025-07-02 22:49:01 +0530

    Leetcode #6

--- LEETCODE/Zigzag.java
+++ LEETCODE/Zigzag.java
@@ -7,17 +7,7 @@ public class Zigzag {
             ans[i] = "";
         }
         int i = 0;
-        while(i<s.length())
-        {
-            for(int index = 0; index < numRows && i< s.length(); index++)
-            {
-                ans[index] += s.charAt(i++);
-            }
-            for(int index = numRows - 2; index > 0 && i<s.length(); index--)
-            {
-                ans[index] += s.charAt(i++);
-            }
-        }
+       
         String res = "";
         for(String str: ans)
         {
