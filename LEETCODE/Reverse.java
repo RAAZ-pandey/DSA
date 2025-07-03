e612032056126ca89c5f1d12fbdf29e6d6f318fa
Rajkumar pandey <Raj.11165@stu.upes.ac.in>
2025-07-03 23:00:20 +0530

    Leetcode #7

--- LEETCODE/Reverse.java
+++ LEETCODE/Reverse.java
@@ -1,3 +1,14 @@
 public class Reverse {
-  
+  public int reverse(int x) {
+        
+        boolean isNegativeNumber = false;
+        if(x < 0)
+        {
+            isNegativeNumber = true;
+            x = -1 * x;
+        }
+
+       
+        return isNegativeNumber ? -1 * result : result;
+    }
 }
