3509a51446e3fe23ac1f8bc375b7acb7f7883426
Rajkumar pandey <Raj.11165@stu.upes.ac.in>
2025-07-06 22:49:39 +0530

    Leetcode #9

--- LEETCODE/Palindrome.java
+++ LEETCODE/Palindrome.java
@@ -3,6 +3,19 @@ public class Palindrome {
         if(x<0){
             return false ;
         }
-      
+        int n = x ;
+        int revNum = 0 ;
+        while(n>0){
+            int d = n%10 ;
+            revNum = revNum*10 + d ;
+            n = n/10 ;
+        }
+        if(revNum == x ){
+            return true ;
+        }
+        else{
+            return false ;
+        }
+    }
 
 }
