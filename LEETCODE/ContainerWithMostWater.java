de602d583918fb0c83f502c8f2654b7a9a9953d2
Rajkumar pandey <Raj.11165@stu.upes.ac.in>
2025-07-07 22:48:52 +0530

    Leetcode #11

--- LEETCODE/ContainerWithMostWater.java
+++ LEETCODE/ContainerWithMostWater.java
@@ -23,3 +23,19 @@ public class ContainerWithMostWater {
   }
 
 }
+
+//solution with least runtime(time Complexity)
+class Solution {
+    public int maxArea(int[] height) 
+    {int a=0,b= height.length-1;
+    int max= 0;
+    while(a<b)
+    {int minV = Math.min(height[a],height[b]); //shorter wall
+    int area = minV*(b-a);  //calculate the water held by container
+    max= Math.max(area,max);
+    while(height[a]<=minV && a<b) a++;
+    while(height[b]<=minV && a<b) b--;
+}
+    return max;
+    }
+}
