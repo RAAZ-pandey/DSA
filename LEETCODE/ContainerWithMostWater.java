3b5817cab66bdfb078cb7631999f151ed6460c52
Rajkumar pandey <Raj.11165@stu.upes.ac.in>
2025-07-07 22:45:35 +0530

    Leetcode #11

--- LEETCODE/ContainerWithMostWater.java
+++ LEETCODE/ContainerWithMostWater.java
@@ -1,3 +1,13 @@
 public class ContainerWithMostWater {
-  
+    public int maxArea(int[] height) {
+
+        int left = 0;
+        int right = height.length - 1;
+        int maxArea = 0;
+
+        
+
+    return maxArea;
+  }
+
 }
