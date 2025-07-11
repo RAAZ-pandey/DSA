05d5d127d9a016f503a0892739ac878875da4a23
Rajkumar pandey <Raj.11165@stu.upes.ac.in>
2025-07-11 23:24:37 +0530

    Leetcode #14

--- LEETCODE/LongestCommonPrefix.java
+++ LEETCODE/LongestCommonPrefix.java
@@ -15,7 +15,7 @@ public class LongestCommonPrefix {
     char[] first = strs[0].toCharArray();
     char[] last = strs[strs.length - 1].toCharArray();
 
-    // Start comparing
+    // Start comparing both strings
     for (int i = 0; i < first.length; i++) {
       if (first[i] != last[i])
         break;
