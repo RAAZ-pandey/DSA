9960920ffa5c278b5d37736b470d4ac366cf32f6
Rajkumar pandey <Raj.11165@stu.upes.ac.in>
2025-07-12 23:31:54 +0530

    Leetcode #15

--- LEETCODE/ThreeSum.java
+++ LEETCODE/ThreeSum.java
@@ -3,6 +3,8 @@ import java.util.*;
 public class ThreeSum {
 
   List<List<Integer>> threeSum(int[] arr) {
+    
+    if (arr == null || arr.length < 3) return new ArrayList<>();
 
     return new ArrayList<>(result);
   }
