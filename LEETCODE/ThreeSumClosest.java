5804d79f8959d2f81d75c4bf344bc7793115e39d
Rajkumar pandey <Raj.11165@stu.upes.ac.in>
2025-07-13 23:23:34 +0530

    Leetcode #16

--- LEETCODE/ThreeSumClosest.java
+++ LEETCODE/ThreeSumClosest.java
@@ -4,7 +4,13 @@ public class ThreeSumClosest {
   
   int threeSumClosest(int[] arr, int target) {
 
-  
+    // Sort the elements
+    Arrays.sort(arr);
+    int resultSum = arr[0] + arr[1] + arr[2];
+    int minDifference = Integer.MAX_VALUE;
+
+       
+    
     return resultSum;
   }
 }
