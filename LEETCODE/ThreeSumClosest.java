8ee0ed6d177159f4f156783d80894094dff0a055
Rajkumar pandey <Raj.11165@stu.upes.ac.in>
2025-07-13 23:24:19 +0530

    Leetcode #16

--- LEETCODE/ThreeSumClosest.java
+++ LEETCODE/ThreeSumClosest.java
@@ -9,8 +9,14 @@ public class ThreeSumClosest {
     int resultSum = arr[0] + arr[1] + arr[2];
     int minDifference = Integer.MAX_VALUE;
 
-       
-    
+    // Now fix the first element and find the other two elements
+    for (int i = 0; i < arr.length - 2; i++) {
+      // Find other two elements using Two Sum approach
+      int left = i + 1;
+      int right = arr.length - 1;
+
+      
+    }
     return resultSum;
   }
 }
