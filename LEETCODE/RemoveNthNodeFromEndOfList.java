90a7c19d6740cfaa6ac68cae99dbeb3ac7397289
Rajkumar pandey <Raj.11165@stu.upes.ac.in>
2025-07-14 23:10:51 +0530

    Leetcode #19

--- LEETCODE/RemoveNthNodeFromEndOfList.java
+++ LEETCODE/RemoveNthNodeFromEndOfList.java
@@ -1,6 +1,11 @@
 public class RemoveNthNodeFromEndOfList {
-  
      ListNode removeNthFromEnd(ListNode head, int n) {
 
+    ListNode dummy = new ListNode(-1);
+    dummy.next = head;
+
+   
+
+    return dummy.next;
   }
 }
