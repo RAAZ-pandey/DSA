e222cd0967ee4929f2c9ec7fa8fad848d5a35135
Rajkumar pandey <Raj.11165@stu.upes.ac.in>
2025-07-18 23:20:22 +0530

    Leetcode #21

--- LEETCODE/MergeTwoSortedList.java
+++ LEETCODE/MergeTwoSortedList.java
@@ -2,7 +2,7 @@ public class MergeTwoSortedList {
   
   public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
 
-    // Create a sentinal/dummy node to start
+    // Create a dummy node to start
     ListNode returnNode = new ListNode(Integer.MIN_VALUE);
 
     // Create a copy of this node to iterate while solving the problem
