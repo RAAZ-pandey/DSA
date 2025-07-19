6835388ec9f00819d6289bcf475e7515db88df4e
Rajkumar pandey <Raj.11165@stu.upes.ac.in>
2025-07-19 23:21:42 +0530

    Leetcode #23

--- LEETCODE/MergeKLists.java
+++ LEETCODE/MergeKLists.java
@@ -5,6 +5,10 @@ public class MergeKLists {
    public ListNode mergeKLists(ListNode[] lists) {
         ListNode head = new ListNode(0);
         ListNode curr = head;
+        Queue<ListNode> minHeap= new PriorityQueue<ListNode>((a, b) -> a.val - b.val);
+        for(ListNode temp : lists){
+            if(temp != null) minHeap.add(temp);
+        }
         
         if(curr != null) curr.next = null;
         return head.next;
