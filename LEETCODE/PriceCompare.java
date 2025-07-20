bebca310edcfca380c8b34e91b343def550c8bdd
Rajkumar pandey <Raj.11165@stu.upes.ac.in>
2025-07-20 23:16:05 +0530

    Capgemini problem

---
+++ LEETCODE/PriceCompare.java
@@ -0,0 +1,16 @@
+
+
+public class PriceCompare {
+    public static int maxProfit(int[] prices) {
+        if (prices.length == 0) return 0;
+        
+        int minPrice = prices[0];
+        int maxProfit = 0;
+      
+        
+        return maxProfit;
+    }
+
+} 
+  
+
