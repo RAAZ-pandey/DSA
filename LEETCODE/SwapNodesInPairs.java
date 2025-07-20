01add1393cde6a2e288143062d6c8ff5031c4994
Rajkumar pandey <Raj.11165@stu.upes.ac.in>
2025-07-20 23:11:05 +0530

    Leetcode #24

--- LEETCODE/SwapNodesInPairs.java
+++ LEETCODE/SwapNodesInPairs.java
@@ -1,7 +1,13 @@
 public class SwapNodesInPairs {
    ListNode swapPairs(ListNode head) {
 
-    
+    // Create a dummy node to ease things
+    ListNode dummy = new ListNode(0);
+    dummy.next = head;
+
+    ListNode point = dummy;
+
+
     // Return the start node
     return dummy.next;
   }
