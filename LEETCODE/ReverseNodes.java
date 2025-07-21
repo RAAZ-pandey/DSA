58d0da28a7f0423a22044fc4ce9db867eb9c8248
Rajkumar pandey <Raj.11165@stu.upes.ac.in>
2025-07-21 22:28:47 +0530

    Leetcode #25

---
+++ LEETCODE/ReverseNodes.java
@@ -0,0 +1,11 @@
+public class ReverseNodes{
+    public ListNode reverseKGroup(ListNode head, int k) {
+         int count =0;
+        ListNode dummy = new ListNode();
+        dummy.next = head;
+       
+            
+        
+        return dummy.next;
+    }
+}
