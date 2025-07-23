e58e96ac8be38ad904096aff4ccb724b78c45d13
Rajkumar pandey <Raj.11165@stu.upes.ac.in>
2025-07-23 10:15:46 +0530

    swapping of elements of arrray

--- LEETCODE/SwapArray.java
+++ LEETCODE/SwapArray.java
@@ -2,18 +2,16 @@ import java.util.Arrays;
 
 public class SwapArray {
   public static void main(String[] args) {
-    int[] arr = {10, 25, 58, 89, 96, 63, 32};
+    int[] arr = { 10, 25, 58, 89, 96, 63, 32 };
     swap(arr, 1, 3);
 
     System.out.println(Arrays.toString(arr));
   }
 
-  static void swap(int[] arr, int index1, int index2){
-        int temp = arr[index1];
-        arr[index1] =arr[index2];
-        arr[index2] = temp;
+  static void swap(int[] arr, int index1, int index2) {
+    int temp = arr[index1];
+    arr[index1] = arr[index2];
+    arr[index2] = temp;
   }
 
-  
-  
 }
