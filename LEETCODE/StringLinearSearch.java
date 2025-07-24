560679aa575842d586ce1cead85b426269bd2ad1
Rajkumar pandey <Raj.11165@stu.upes.ac.in>
2025-07-24 14:29:21 +0530

    Linear search in s string for ant charactrer

--- LEETCODE/StringLinearSearch.java
+++ LEETCODE/StringLinearSearch.java
@@ -3,9 +3,23 @@ public class StringLinearSearch {
       String s = "ribhubhushan";
 
       System.out.println(Search(s, 'n'));
-
+      System.out.println(Search1(s, 'n'));
     }
 
+     static boolean Search1(String str, char element){
+       if(str.length() == 0){
+        return false;
+      }
+
+        for (char ch : str.toCharArray()){
+            if(ch == element){
+              return true;
+            }
+        }    
+      
+      return false;
+       
+    }
 
     static boolean Search(String str, char element){
        if(str.length() == 0){
