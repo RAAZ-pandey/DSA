ba5fcdc2c0465c7df186437be160bd737c3474f5
Rajkumar pandey <Raj.11165@stu.upes.ac.in>
2025-07-24 14:43:53 +0530

    searching for the minimum value in array

---
+++ LEETCODE/MinMaxArray.java
@@ -0,0 +1,23 @@
+public class MinMaxArray {
+  public static void main(String[] args) {
+    int[] arr = { 10, 25, 58, -7, 89, 96, 63, 13, 32 };
+    System.out.println(min(arr));
+  }
+
+
+// check and return the minimum value in array
+  static int min(int[] arr){
+    if(arr.length == 0){
+      return -1;
+    }
+
+      int ans = arr[0];
+      for (int i = 0; i < arr.length; i++) {
+      if(arr[i] < ans){
+        ans = arr[i];
+      }
+    }
+    return ans;
+  }
+  
+}
