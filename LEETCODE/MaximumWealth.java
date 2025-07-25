2fc87607bbc23d07fb44ff9ae4072f4e194ecb37
Rajkumar pandey <Raj.11165@stu.upes.ac.in>
2025-07-25 23:30:40 +0530

    Leetcode #1672

--- LEETCODE/MaximumWealth.java
+++ LEETCODE/MaximumWealth.java
@@ -1,3 +1,18 @@
 public class MaximumWealth {
-  
+  public static void main(String[] args) {
+    
+  }
+   static int maximumWealth(int[][] accounts) {
+        int ans =Integer.MIN_VALUE;
+        for(int[] ints: accounts){
+            int sum = 0;
+            for( int col : ints){
+                sum += col;
+            }
+            if(sum > ans){
+                ans = sum;
+            }
+        }
+        return ans;
+    }
 }
