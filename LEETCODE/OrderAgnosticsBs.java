465155855159fdd89b11073254df8690685976f7
Rajkumar pandey <Raj.11165@stu.upes.ac.in>
2025-07-26 00:01:39 +0530

    Binary search in an array without knowing the sorting order

--- LEETCODE/OrderAgnosticsBs.java
+++ LEETCODE/OrderAgnosticsBs.java
@@ -1,7 +1,7 @@
 public class OrderAgnosticsBs {
   public static void main(String[] args) {
     int[] arr = { -17, -12, -5, 0, 3, 16, 34, 47, 58, 68, 99, 225 };
-    int ans = binarysearch(arr, 0);
+    int ans = binarysearch(arr, 16);
     System.out.println(ans);
   }
 
