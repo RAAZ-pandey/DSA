089e8a311628a1c39b9535b3756f2a270d5a29de
Rajkumar pandey <Raj.11165@stu.upes.ac.in>
2025-07-26 12:31:59 +0530

    searching floor of a number if  the number itself is not present in array through Binary search

---
+++ LEETCODE/FloorBs.java
@@ -0,0 +1,32 @@
+public class FloorBs {
+  public static void main(String[] args) {
+    int[] arr = {-17, -12, -5, 0, 3, 16, 34, 47, 58, 68, 99, 225};
+    int ans = search(arr, 1);
+    System.out.println(ans);
+   }
+
+   static int search(int[] arr, int target){
+
+    int start = 0;
+    int end = arr.length - 1;
+
+    while (start <= end) {
+      //find the middle element of array
+     // int mid = (start + end) / 2;
+      // sometime the value of mid can exceed the limit of integer value (possible)
+      //tacling that edge case
+      int mid = start + (end - start) / 2;
+
+      if (target < arr[mid]) {
+        end = mid - 1;
+      } else if (target > arr[mid]) {
+          start = mid + 1;
+      }else 
+      // target found
+        return mid;
+
+    }
+  //  if target not found then returnibg the largest element smaller than target
+    return end;
+  }
+}
