b2350e57910dd363f01bbc63dadde9d04b04d28b
Rajkumar pandey <Raj.11165@stu.upes.ac.in>
2025-07-27 21:43:56 +0530

    added comments

--- LEETCODE/PeakIndexInMountainArray.java
+++ LEETCODE/PeakIndexInMountainArray.java
@@ -7,11 +7,13 @@ public class PeakIndexInMountainArray {
            int mid = start + (end - start) / 2;
 
             if(arr[mid] > arr [mid + 1]) {
+              //you are in decending part of the array
+              //it may be probable answer
                 end = mid;
             } else {
-                start = mid + 1;
+                start = mid + 1;  // or this is the answer if you are in acending part of the array
             }
         }
-        return start;
+        return start; //here start and end both can be the answer as both indicate the same value
     }
 }
