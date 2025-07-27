bd17bf668c0d1fc64e8dbef81b9d5c05892f7734
Rajkumar pandey <Raj.11165@stu.upes.ac.in>
2025-07-27 15:30:08 +0530

    GeekforGeeks problem

--- LEETCODE/InfiniteArraySearch.java
+++ LEETCODE/InfiniteArraySearch.java
@@ -16,4 +16,29 @@ public class InfiniteArraySearch {
       }
       return -1;
     }
+
+    static int binarysearch(int[] arr, int target){
+
+    int start = 0;
+    int end = arr.length - 1;
+
+    while (start <= end) {
+      //find the middle element of array
+     // int mid = (start + end) / 2;
+      // sometime the value of mid can exceed the limit of integer value (possible)
+      //tacling that edge case
+      int mid = start + (end - start) / 2;
+
+      if (target < arr[mid]) {
+        end = mid - 1;
+      } else if (target > arr[mid]) {
+          start = mid + 1;
+      }else 
+      // target found
+        return mid;
+
+    }
+
+    return -1;
+   }
 }
