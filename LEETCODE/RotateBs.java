bf44434accb6f2d474d565eae3b855fa62ee3bf7
Rajkumar pandey <Raj.11165@stu.upes.ac.in>
2025-07-28 15:32:06 +0530

    LeetCode #33

--- LEETCODE/RotateBs.java
+++ LEETCODE/RotateBs.java
@@ -1,6 +1,21 @@
 public class RotateBs {
 
 
+    static int binarySearch(int[] arr, int target, int start, int end) {
+        while(start <= end) {
+            int mid = start + (end - start) / 2;
+
+            if (target < arr[mid]) {
+                end = mid - 1;
+            } else if (target > arr[mid]) {
+                start = mid + 1;
+            } else {
+                // ans found
+                return mid;
+            }
+        }
+        return -1;
+    }
 
     // this will not work in duplicate values
     static int findPivot(int[] arr) {
