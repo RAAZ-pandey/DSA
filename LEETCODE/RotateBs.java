f2f7e972463bb226bec75bf7e7b9093438fbd2d6
Rajkumar pandey <Raj.11165@stu.upes.ac.in>
2025-07-28 15:31:42 +0530

    LeetCode #33

---
+++ LEETCODE/RotateBs.java
@@ -0,0 +1,26 @@
+public class RotateBs {
+
+
+
+    // this will not work in duplicate values
+    static int findPivot(int[] arr) {
+        int start = 0;
+        int end = arr.length - 1;
+        while (start <= end) {
+            int mid = start + (end - start) / 2;
+            // 4 cases over here
+            if (mid < end && arr[mid] > arr[mid + 1]) {
+                return mid;
+            }
+            if (mid > start && arr[mid] < arr[mid - 1]) {
+                return mid-1;
+            }
+            if (arr[mid] <= arr[start]) {
+                end = mid - 1;
+            } else {
+                start = mid + 1;
+            }
+        }
+        return -1;
+    }
+}
