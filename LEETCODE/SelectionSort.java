12339e68d4d39d555e63f503c4cc368adccfaeee
Rajkumar pandey <Raj.11165@stu.upes.ac.in>
2025-07-29 13:50:25 +0530

    selection sort

--- LEETCODE/SelectionSort.java
+++ LEETCODE/SelectionSort.java
@@ -12,10 +12,15 @@ public class SelectionSort {
      for (int i = 0; i < arr.length; i++) {
         int last = arr.length - i - 1;
         int maxIndex = getMaxIndex(arr, 0, last);
-
+        swap(arr, maxIndex, last);
      }
   }
 
+  static void swap(int[] arr, int first, int second) {
+    int temp = arr[first];
+    arr[first] = arr[second];
+    arr[second] = temp;
+  }
 
   static int getMaxIndex(int[] arr, int start, int end) {
      int max =start;
