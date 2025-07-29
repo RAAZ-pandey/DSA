176d92c2de5438859de5c21c34d5c780b61cf980
Rajkumar pandey <Raj.11165@stu.upes.ac.in>
2025-07-29 13:50:03 +0530

    selection sort

--- LEETCODE/SelectionSort.java
+++ LEETCODE/SelectionSort.java
@@ -1,3 +1,31 @@
+import java.util.Arrays;
+
 public class SelectionSort {
+  public static void main(String[] args) {
+     int[] arr = {5, 4, 3, 8, 1, -15, -1, 0, 87, -54};
+    sorting(arr);
+    System.out.println(Arrays.toString(arr));
+
+  }
+
+ static void sorting(int[] arr) {
+     for (int i = 0; i < arr.length; i++) {
+        int last = arr.length - i - 1;
+        int maxIndex = getMaxIndex(arr, 0, last);
+
+     }
+  }
+
+
+  static int getMaxIndex(int[] arr, int start, int end) {
+     int max =start;
+
+      for (int i = start; i <= end; i++) {
+        if(arr[max] < arr[i]) {
+          max = i;
+        }
+      }
+     return max;
+  }
   
 }
