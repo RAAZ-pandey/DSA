522a0a1dd537fa11da8cb5a559de2cf65d006387
Rajkumar pandey <Raj.11165@stu.upes.ac.in>
2025-07-29 13:20:40 +0530

    Bubblr sort

---
+++ LEETCODE/BubbleSort.java
@@ -0,0 +1,29 @@
+
+import java.util.Arrays;
+
+public class BubbleSort {
+  public static void main(String[] args) {
+    int[] arr = {5, 4, 3, 8, 1, -15, -1, 0, 87, -54};
+    sorting(arr);
+    System.out.println(Arrays.toString(arr));
+
+  }
+
+  static void sorting(int[] arr) {
+
+    for (int i = 0; i < arr.length; i++) {
+      for (int j = 1; j < arr.length - i; j++) {
+        // swap if element is smaller than previos element
+        if (arr[j] < arr[j - 1]) {
+          // swap
+          int temp = arr[j];
+          arr[j] = arr[j - 1];
+          arr[j - 1] = temp;
+       
+        }
+      }
+   
+      
+    }
+  }
+}
