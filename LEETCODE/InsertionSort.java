421fca84c39c743119ee1e3908ea4717d871ce27
Rajkumar pandey <Raj.11165@stu.upes.ac.in>
2025-07-29 19:59:54 +0530

    insertion sort

--- LEETCODE/InsertionSort.java
+++ LEETCODE/InsertionSort.java
@@ -11,7 +11,7 @@ public class InsertionSort {
    static void sort(int[] arr) {
 
     for (int i = 0; i < arr.length - 1; i++) {
-      for (int j = 1; j > 0; j--) {
+      for (int j = i + 1; j > 0; j--) {
         if (arr[j] < arr[j-1]) {
           swap(arr, j, j-1);
         } else {
