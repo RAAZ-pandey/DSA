f5b30d58cd4d87257e7155794978622c05d2a380
Rajkumar pandey <Raj.11165@stu.upes.ac.in>
2025-07-29 13:22:40 +0530

    reduce time cimplexity in case array is sorted already

--- LEETCODE/BubbleSort.java
+++ LEETCODE/BubbleSort.java
@@ -11,6 +11,7 @@ public class BubbleSort {
 
   static void sorting(int[] arr) {
 
+    boolean swapped = false;
     for (int i = 0; i < arr.length; i++) {
       for (int j = 1; j < arr.length - i; j++) {
         // swap if element is smaller than previos element
@@ -19,11 +20,12 @@ public class BubbleSort {
           int temp = arr[j];
           arr[j] = arr[j - 1];
           arr[j - 1] = temp;
-       
+          swapped = true;
         }
       }
-   
-      
-    }
+    if (!swapped) {
+      break;
+    }    
+   }
   }
 }
