b98f7fa4e1591504c3c03fd47fbd8c548179180e
Rajkumar pandey <Raj.11165@stu.upes.ac.in>
2025-08-02 00:48:03 +0530

    LeetCode #287

--- LEETCODE/FindDuplicate.java
+++ LEETCODE/FindDuplicate.java
@@ -1,14 +1,39 @@
 public class FindDuplicate {
 
   // Best and efficient solution
-    public int findDuplicate(int[] nums) {
-          int[] ans = new int[nums.length];
-        for(int i: nums){
-            if(ans[i] == -1){
-                return i;
+    // public int findDuplicate(int[] nums) {
+    //       int[] ans = new int[nums.length];
+    //     for(int i: nums){
+    //         if(ans[i] == -1){
+    //             return i;
+    //         }
+    //         ans[i] = -1;
+    //     }
+    //     return -1;
+    // }
+
+    // using cycle sort method
+     public int findDuplicate(int[] nums) {
+        int i = 0;
+
+        while (i < nums.length) {
+            if (nums[i] != i + 1 ) {
+                int check = nums[i] - 1;
+                if(nums[i] != nums[check]) {
+                    swap(nums, i, check);
+                } else {
+                    return nums[i];
+                }
+            }else {
+                i++;
             }
-            ans[i] = -1;
         }
         return -1;
+    }
+
+    void swap(int[] arr, int i, int j) {
+        int temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
     }
 }
