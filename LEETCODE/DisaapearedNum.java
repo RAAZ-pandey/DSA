69f1bf0e27457370d62372b95781ff8402cae6e6
Rajkumar pandey <Raj.11165@stu.upes.ac.in>
2025-08-01 15:12:18 +0530

    LeetCode #448

--- LEETCODE/DisaapearedNum.java
+++ LEETCODE/DisaapearedNum.java
@@ -1,3 +1,30 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class DisaapearedNum {
-  
+    public List<Integer> findDisappearedNumbers(int[] nums) {
+    int i =0;
+    while (i < nums.length) {
+      int check = nums[i] - 1;
+      if( nums[i] != nums[check]) {
+          swap(nums, i, check);
+      } else {
+        i++;
+      }
+    } 
+
+    List<Integer> ans = new ArrayList<>();
+    for (int index = 0; index < nums.length; index++) {
+        if(nums[index] != index + 1) {
+            ans.add(index + 1);
+        }
+    }
+    return ans;
+  }
+
+  void swap(int[] arr, int i, int check) {
+  int temp = arr[i];
+  arr[i] = arr[check];
+  arr[check] = temp;
+ }
 }
