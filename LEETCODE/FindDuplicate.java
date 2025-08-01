c6e55b12ed0b6e134531bdef7c9cff93e3ea32c9
Rajkumar pandey <Raj.11165@stu.upes.ac.in>
2025-08-02 00:46:23 +0530

    LeetCode #287

--- LEETCODE/FindDuplicate.java
+++ LEETCODE/FindDuplicate.java
@@ -1,3 +1,14 @@
 public class FindDuplicate {
-  
+
+  // Best and efficient solution
+    public int findDuplicate(int[] nums) {
+          int[] ans = new int[nums.length];
+        for(int i: nums){
+            if(ans[i] == -1){
+                return i;
+            }
+            ans[i] = -1;
+        }
+        return -1;
+    }
 }
