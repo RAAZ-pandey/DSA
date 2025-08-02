a869cb9889f0683ed02c1218bf7cb81a8d130dd4
Rajkumar pandey <Raj.11165@stu.upes.ac.in>
2025-08-02 23:46:49 +0530

    LeetCode #41

--- LEETCODE/MissingPositive.java
+++ LEETCODE/MissingPositive.java
@@ -1,3 +1,26 @@
 public class MissingPositive {
-  
+    public int firstMissingPositive(int[] nums) {
+         int i = 0;
+        while(i < nums.length) {
+            int check = nums[i] - 1;
+            if (nums[i] > 0 &&  nums[i] <= nums.length && nums[i] != nums[check]) {
+                swap(nums, i, check);
+            } else {
+                i++;
+            }
+        }
+        for(int index = 0; index < nums.length; index++) {
+            if(nums[index] != index + 1){
+                return index + 1;
+            }
+        }
+        return nums.length + 1;
+    }
+
+    void swap(int[] arr, int i, int j) {
+        int temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
+    }
+    
 }
