4b49f69c0e9baa04920aea06d4c06ce652726f67
Rajkumar pandey <Raj.11165@stu.upes.ac.in>
2025-08-02 23:14:58 +0530

    LeetCode #645

--- LEETCODE/Mismatch.java
+++ LEETCODE/Mismatch.java
@@ -1,3 +1,14 @@
 public class Mismatch {
+   public int[] findErrorNums(int[] nums) {
+        int i = 0;
+        while(i < nums.length) {
+            int check = nums[i] - 1;
+            if (nums[i] != nums[check]) {
+                swap(nums, i, check);
+            } else {
+                i++;
+            }
+        }
+      
   
 }
