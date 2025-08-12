1c785d886f7344e7bccc7bb52d4b69c5a72bf2e0
Rajkumar pandey <Raj.11165@stu.upes.ac.in>
2025-08-12 18:21:59 +0530

    practise pattern problems

---
+++ LEETCODE/Solution.java
@@ -0,0 +1,14 @@
+public class Solution {
+   public static void main(String[] args) {
+    patterns1(4);
+   }
+
+   static void patterns1(int num) {
+      for (int i = 0; i<= num; i++) {
+        for (int j = 0; j < i ; j++) {
+           System.out.print("*");
+        }
+        System.out.println("");
+      }
+   }
+}
