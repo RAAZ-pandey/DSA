03ce4b6c5c89157229ff6edf89db53a791925e60
Rajkumar pandey <Raj.11165@stu.upes.ac.in>
2025-08-14 17:15:45 +0530

    Bitwise operator

---
+++ Recursion/IsOdd.java
@@ -0,0 +1,11 @@
+package Recursion;
+
+public class IsOdd {
+  public static void main(String[] args) {
+     int n = 64;
+    System.out.println(isOdd(n));
+  }
+  static boolean isOdd(int n) {
+    return (n & 1) == 1;
+  }
+}
