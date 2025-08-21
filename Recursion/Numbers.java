acb5d3e00d6aa1a005d60f660bc82dca48ff5588
Rajkumar pandey <Raj.11165@stu.upes.ac.in>
2025-08-22 00:33:51 +0530

    Printing numbers in both ascending and decending using Recursion

--- Recursion/Numbers.java
+++ Recursion/Numbers.java
@@ -2,27 +2,38 @@ package Recursion;
 
 public class Numbers {
   public static void main(String[] args) {
-    nums(8);
-    System.out.println("now print in ascending");
-    Anums(8);
+   // nums(8);
+   // System.out.println("now print in ascending");
+  // Anums(8);
+   nums(8);
   }
 
-  static void nums(int n) {
+  // static void nums(int n) {
+    
+  //   if (n == 0) {
+  //     return;
+  //   }
+  //   System.out.println(n);
+  //   nums(n - 1);
+  // }
+
+  //   static void Anums(int n) {
+    
+  //   if (n == 0) {
+  //     return;
+  //   }
+
+  //   Anums(n - 1);
+  //   System.out.println(n);
+  // }
+
+    static void nums(int n) {
     
     if (n == 0) {
       return;
     }
     System.out.println(n);
     nums(n - 1);
-  }
-
-    static void Anums(int n) {
-    
-    if (n == 0) {
-      return;
-    }
-
-    Anums(n - 1);
     System.out.println(n);
   }
 }
