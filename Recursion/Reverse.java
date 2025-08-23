aa2dabd5fa535f46a45d6c069d2708bbcd0e8df7
Rajkumar pandey <Raj.11165@stu.upes.ac.in>
2025-08-23 14:40:57 +0530

    reverse of a number

--- Recursion/Reverse.java
+++ Recursion/Reverse.java
@@ -11,8 +11,20 @@ public class Reverse {
      reverse2(n / 10);
    }
 
+   static int reverse1(int n) {
+    int digits = (int)(Math.log10(n)) + 1;
+    return helper(n, digits);
+   }
+
+   private static int helper(int n, int digits) {
+     if (n % 10 == n) {
+      return n;
+     }
+
+     int rem = n % 10;
+     return rem * (int)(Math.pow(10, digits - 1)) + helper(n / 10, digits - 1);
+   }
    public static void main(String[] args) {
-    reverse2(13456);
-    System.out.println(sum);
+    System.out.println(reverse1(13456));
    }
 }
