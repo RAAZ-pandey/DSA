62521540276a1027bcfe7552dac834ce9156c550
Rajkumar pandey <Raj.11165@stu.upes.ac.in>
2025-08-24 20:33:25 +0530

    linear search for of all elements

--- Recursion/LinearsList.java
+++ Recursion/LinearsList.java
@@ -1,5 +1,28 @@
 package Recursion;
 
+import java.util.ArrayList;
+
 public class LinearsList {
+   public static void main(String[] args) {
+    int[] arr = {-15, -5, 0, 1, 2, 6, 6, 11, 15};
+    int target = 6;
+
+    ArrayList<Integer> ans = searchAll(arr, target, 0, new ArrayList<>());
+    System.out.println(ans);
+  }
   
+
+ 
+
+ 
+  static ArrayList<Integer> searchAll(int[] arr, int target, int index, ArrayList<Integer> list ) {
+     if(index == arr.length){
+      return list;
+    }
+
+    if(arr[index] == target){
+       list.add(index);
+    }
+    return searchAll(arr, target, index + 1, list);
+  }
 }
