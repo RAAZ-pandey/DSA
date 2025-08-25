e3c31a6ae1c5e36d2d4484771272d7769b1a1190
Rajkumar pandey <Raj.11165@stu.upes.ac.in>
2025-08-25 23:52:15 +0530

    Rotated binary search uusing recursion

--- Recursion/RotatedBS.java
+++ Recursion/RotatedBS.java
@@ -2,7 +2,8 @@ package Recursion;
 
 public class RotatedBS {
   public static void main(String[] args) {
-    
+     int[] arr = {5, 6, 7, 8, 9, 1, 2, 3, 4};
+     System.out.println(search(arr, 3, 0, arr.length - 1));
   }
   
   static int search(int[] arr, int target, int start, int end){
