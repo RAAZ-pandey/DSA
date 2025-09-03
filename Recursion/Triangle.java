fcca9c1534ec869bfc5ff9223cc3c063eb8d4b24
Rajkumar pandey <Raj.11165@stu.upes.ac.in>
2025-09-04 01:33:58 +0530

    Printing traingle pattern with recursion

--- Recursion/Triangle.java
+++ Recursion/Triangle.java
@@ -1,5 +1,22 @@
 package Recursion;
 
 public class Triangle {
-  
+  public static void main(String[] args) {
+    print(5, 0);
+    
+  }
+
+  static void print(int r, int c) {
+    if (r == 0) {
+      return;
+    }
+
+    if(c < r) {
+      System.out.println("*");
+      print(r, c+1);
+    } else {
+      System.out.println();
+      print(r-1, 0);
+    }
+  }
 }
