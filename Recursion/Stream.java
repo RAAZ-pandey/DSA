e805017aa28cf6700af891a3efdbfb5767753ced
Rajkumar pandey <Raj.11165@stu.upes.ac.in>
2025-09-07 16:07:47 +0530

    remove specific character from string

--- Recursion/Stream.java
+++ Recursion/Stream.java
@@ -3,8 +3,10 @@ package Recursion;
 public class Stream {
   public static void main(String[] args) {
     skip("", "baaoasaas");
+    System.out.println(skip1("aaRaiabaihaaaua"));
   }
 
+  // printing the answer string after skiping a specific character
   static void skip(String p, String q) {
     if(q.isEmpty()){
       System.out.println(p);
@@ -17,6 +19,22 @@ public class Stream {
       skip(p, q.substring(1));
     } else {
       skip(p + ch, q.substring(1));
+    }
+  }
+
+  // returning answer after skiping a specific character
+  
+  static String skip1(String q) {
+    if(q.isEmpty()){
+      return "";
+    }
+
+    char ch = q.charAt(0);
+
+    if (ch == 'a') {
+      return skip1(q.substring(1));
+    } else {
+      return ch + skip1(q.substring(1));
     }
   }
 }
