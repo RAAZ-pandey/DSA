1f44d025879d2016bd44dad95909fadeac52ad58
Rajkumar pandey <Raj.11165@stu.upes.ac.in>
2025-09-08 18:43:43 +0530

    returning all Permutations of a string in arraylist

--- Recursion/Permutation.java
+++ Recursion/Permutation.java
@@ -1,7 +1,10 @@
 package Recursion;
 
+import java.util.ArrayList;
+
 public class Permutation {
    public static void main(String[] args) {
+    // permutations("", "abc");
     permutations("", "abc");
    }
   
@@ -17,4 +20,23 @@ public class Permutation {
             permutations(f + ch + s, up.substring(1));
         }
     }
+
+     static ArrayList<String> permutationsList(String p, String up) {
+        if (up.isEmpty()) {
+            ArrayList<String> list = new ArrayList<>();
+            list.add(p);
+            return list;
+        }
+        char ch = up.charAt(0);
+
+        // local to this call
+        ArrayList<String> ans = new ArrayList<>();
+
+        for (int i = 0; i <= p.length(); i++) {
+            String f = p.substring(0, i);
+            String s = p.substring(i, p.length());
+            ans.addAll(permutationsList(f + ch + s, up.substring(1)));
+        }
+        return ans;
+    } 
 }
