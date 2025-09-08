0439847429081906f744eec9da206b9b572c6ea9
Rajkumar pandey <Raj.11165@stu.upes.ac.in>
2025-09-08 21:10:54 +0530

    leetcode 11

--- Recursion/PhonePad.java
+++ Recursion/PhonePad.java
@@ -1,5 +1,7 @@
 package Recursion;
 
+import java.util.ArrayList;
+
 public class PhonePad {
    
    static void pad(String p, String up) {
@@ -12,6 +14,23 @@ public class PhonePad {
             char ch = (char) ('a' + i);
             pad(p + ch, up.substring(1));
         }
-
     }
+
+   static ArrayList<String> padRet(String p, String up) {
+        if (up.isEmpty()) {
+            ArrayList<String> list = new ArrayList<>();
+            list.add(p);
+            return list;
+        }
+        int digit = up.charAt(0) - '0'; // this will convert '2' into 2
+
+        ArrayList<String> list = new ArrayList<>();
+
+        for (int i = (digit - 1) * 3; i < digit * 3; i++) {
+            char ch = (char) ('a' + i);
+            list.addAll(padRet(p + ch, up.substring(1)));
+        }
+        return list;
+    }
+
 }
