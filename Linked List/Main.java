090bcf6f7728c3cddf1ba063411c38501e7bfb9b
Rajkumar pandey <Raj.11165@stu.upes.ac.in>
2025-09-14 23:17:59 +0530

    calling inset method in linked list

--- Linked List/Main.java
+++ Linked List/Main.java
@@ -1,12 +1,13 @@
 public class Main {
     public static void main(String[] args) {
         SLL list = new SLL();
-         list.insertFirst(3);
+        list.insertFirst(3);
         list.insertFirst(2);
         list.insertFirst(8);
         list.insertFirst(17);
         list.insertLast(19);
+        list.insert(100, 3);
 
-         list.display();
+        list.display();
     }
 }
