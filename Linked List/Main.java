46a36b172e4e5de8e148811f7faafbc01ddc88da
Rajkumar pandey <Raj.11165@stu.upes.ac.in>
2025-09-14 23:04:45 +0530

    calling inserion at first

--- Linked List/Main.java
+++ Linked List/Main.java
@@ -1,5 +1,9 @@
 public class Main {
     public static void main(String[] args) {
         SLL list = new SLL();
+         list.insertFirst(3);
+        list.insertFirst(2);
+        list.insertFirst(8);
+        list.insertFirst(17);
     }
 }
