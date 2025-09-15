4dcd022b9c48ff2c93dfa03c8f62300ab8d876db
Rajkumar pandey <Raj.11165@stu.upes.ac.in>
2025-09-15 19:51:50 +0530

    calling delete method in linked list

--- Linked List/Main.java
+++ Linked List/Main.java
@@ -8,12 +8,14 @@ public class Main {
         list.insertLast(19);
         list.insert(100, 3);
 
-        // list.display();
-        // list.insertRec(88, 2);
         list.display();
         System.out.println(list.deleteFirst());
         list.display();
         System.out.println(list.deleteLast());
+        list.display();
+         System.out.println(list.delete(2));
+        list.display();
+        list.insertRec(88, 2);
         list.display();
     }
 }
