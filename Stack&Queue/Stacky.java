65bba279276c2ac98518263aa77a08118bdfa72f
Rajkumar pandey <Raj.11165@stu.upes.ac.in>
2025-10-02 14:24:15 +0530

    pushing elements in a stack

---
+++ Stack&Queue/Stacky.java
@@ -0,0 +1,20 @@
+import java.util.Stack;
+
+public class Stacky {
+  public static void main(String[] args) {
+      Stack<Integer> stack = new Stack<>();
+
+      stack.push(95);
+      stack.push(85);
+      stack.push(75);
+      stack.push(65);
+      stack.push(55);
+      stack.push(45);
+      stack.push(35);
+
+      
+      
+
+  }
+
+}
