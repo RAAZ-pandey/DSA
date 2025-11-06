7a827f66ce72e3c10e790c6f562d91cefcb8cf56
Rajkumar pandey <Raj.11165@stu.upes.ac.in>
2025-11-06 19:48:22 +0530

    custom queue implemetation

---
+++ StackQueue/QueueMain.java
@@ -0,0 +1,17 @@
+package StackQueue;
+
+public class QueueMain {
+    public static void main(String[] args) throws Exception {
+        CustomQueue queue = new CustomQueue(5);
+        queue.insert(3);
+        queue.insert(13);
+        queue.insert(23);
+        queue.insert(33);
+        queue.insert(43);
+
+        queue.display();
+
+        System.out.println(queue.remove());
+        queue.display();
+    }
+}
