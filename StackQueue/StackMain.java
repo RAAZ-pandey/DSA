3c48d20690a0ae72f84d2effb726a50eff80ce57
Rajkumar pandey <Raj.11165@stu.upes.ac.in>
2025-11-06 14:49:27 +0530

    custom stack implemetation with dynamic memory

--- StackQueue/StackMain.java
+++ StackQueue/StackMain.java
@@ -3,6 +3,8 @@ package StackQueue;
 public class StackMain {
     public static void main(String[] args) throws StackException{
       CustomStack stack = new DynamicStack(5);
+     // CustomStack stack = new CustomStack(5);
+     // DynamicStack stack = new DynamicStack(5);
 
       stack.push(95);
       stack.push(85);
