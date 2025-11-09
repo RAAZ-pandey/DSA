13ac369a5202645af9289c7bfb37ce1fa6396535
Rajkumar pandey <Raj.11165@stu.upes.ac.in>
2025-11-09 22:57:30 +0530

    Check if a given Year is Leap Year

---
+++ Numbers/LeapYear.java
@@ -0,0 +1,17 @@
+package Numbers;
+
+import java.util.Scanner;
+
+public class LeapYear {
+   public static void main(String[] args) {
+    //int year = 1900;
+    Scanner sc = new Scanner(System.in);
+    int year = sc.nextInt();
+
+    if ((year % 400 == 0) || ( year % 4 == 0 && year % 100 != 0)) {
+      System.out.println(year + " is leap year");
+    } else {
+      System.out.println(year + " is not a leap year");
+    }
+   }
+}
