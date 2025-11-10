8b9068b4205ccb164eb9160952ab1738ab02800b
Rajkumar pandey <Raj.11165@stu.upes.ac.in>
2025-11-10 17:40:48 +0530

    LEETCODE  367

---
+++ LEETCODE/PerfectSquare.java
@@ -0,0 +1,22 @@
+public class PerfectSquare {
+  public Boolean isPerfectSquare (int num){
+    if (num < 0) return false;
+    if(num ==0 && num == 1) return true;
+
+   long low = 1, high = num / 2;
+
+   while(low <= high) {
+        long mid = low + (high - low) /2;
+	long square = mid * mid;
+
+	if ( square == num) {
+             return true;
+	} else if (square < num) {
+	     low = mid + 1;
+	} else {
+	    high = mid - 1;
+	}
+      }
+    return false;
+  }
+}
